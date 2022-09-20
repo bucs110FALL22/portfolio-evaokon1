47ff839779cbb19e6a5666b3fd42b227ad43fead
evaokon1 <112992795+evaokon1@users.noreply.github.com>
2022-09-20 03:04:28 +0000

    I did the exercise

---
+++ ch03/exercises/guess.py
@@ -0,0 +1,10 @@
+import random
+num = random.randint(1, 10)
+for i in range(3):
+  guess = int(input("Enter a number: "))
+  if guess == num:
+    print("Correct"
+  if guess < num:
+    print("Too low")
+  if guess > num:
+    print("Too high")
